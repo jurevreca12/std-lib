57f2e485d8fe5e4f75dd5107be292b7db4a18630
jvreca <jure.vreca@ijs.si>
2026-07-14 08:21:01 +0000

    Updated cntr to use wclear

--- rtl/cntr.sv
+++ rtl/cntr.sv
@@ -4,19 +4,21 @@ module cntr #(
 )(
     input  logic clk,
     input  logic rstn,
+    input  logic clear,
     input  logic ce,
     output logic [WORD_WIDTH-1:0] count
 );
     logic [WORD_WIDTH-1:0] plus_one;
-    register #(
+    register_wclear #(
         .DTYPE(logic [WORD_WIDTH-1:0]),
         .RESET_VALUE(RESET_VALUE)
     ) rvfi_order_reg (
-        .clk (clk),
-        .rstn(rstn),
-        .ce  (ce),
-        .in  (plus_one),
-        .out (count)
+        .clk  (clk),
+        .rstn (rstn),
+	.clear(clear)
+        .ce   (ce),
+        .in   (plus_one),
+        .out  (count)
     );
     assign plus_one = count + 1'b1;
 endmodule
