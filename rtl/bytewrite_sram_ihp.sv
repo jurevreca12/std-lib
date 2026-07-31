efa9efe53bd01c9e8c31c14bf972f66e2d3f54dd
carotium <coolboy9987@gmail.com>
2026-07-31 13:35:03 +0200

    Removed sram_impl, which was a wrapper for bytewrite, now that the modules are separated.

--- rtl/bytewrite_sram_ihp.sv
+++ rtl/bytewrite_sram_ihp.sv
@@ -1,46 +1,84 @@
-// Single-Port BRAM with Byte-wide Write Enable
-// Read-First mode
-// Single-process description
-// Compact description of the write with a generate-for 
-//   statement
-// Column width and number of columns easily configurable
-//
-// bytewrite_ram_1b.v
-//
-
 module bytewrite_sram #(
-    parameter  int    WORD_SIZE=32,
-    parameter         MEM_INIT_FILE="",
-    parameter  int    INIT_FILE_BIN=1,
-    parameter  int    MEM_SIZE_WORDS = 2048,
-    localparam int    AddrWidth = $clog2(MEM_SIZE_WORDS),
-    localparam int    NBytes = (WORD_SIZE / 8)
-)(
-    input                        clk,
-    input  logic [NBytes-1:0]    strobe,
-    input  logic                 write,
-    input  logic                 valid,
-    input  logic [AddrWidth-1:0] addr,
-    input  logic [WORD_SIZE-1:0] din,
-    output logic [WORD_SIZE-1:0] dout
+  parameter   int     WORD_SIZE=32,
+  parameter           MEM_INIT_FILE="",
+  parameter   int     INIT_FILE_BIN=1,
+  parameter   int     MEM_SIZE_WORDS = 2048,
+  // Dependent parameters, do not overwrite
+  localparam  int     AddrWidth = $clog2(MEM_SIZE_WORDS),
+  localparam  int     NBytes = (WORD_SIZE / 8)
+) (
+  input                        clk,
+  input  logic [NBytes-1:0]    strobe,
+  input  logic                 write,
+  input  logic                 valid,
+  input  logic [AddrWidth-1:0] addr,
+  input  logic [WORD_SIZE-1:0] din,
+  output logic [WORD_SIZE-1:0] dout
 );
 
-logic [WORD_SIZE-1:0] RAM [MEM_SIZE_WORDS];
+  // Generate bit mask for interleaving 2*32'b into 64'b
+  logic [WORD_SIZE-1:0] bm;
+  logic addr_lsb;
+  for(genvar i = 0; i < WORD_SIZE; ++i) begin: gen_bm_ports
+    assign bm[i] = strobe[i/8];
+  end
 
-  sram_impl #(
-    .WORD_SIZE     (WORD_SIZE),
-    .MEM_INIT_FILE (MEM_INIT_FILE),
-    .INIT_FILE_BIN (INIT_FILE_BIN),
-    .MEM_SIZE_WORDS(MEM_SIZE_WORDS)
-  ) sram_impl_i (
-    .clk   (clk),
-    .strobe(strobe),
-    .write (write),
-    .valid (valid),
-    .addr  (addr),
-    .din   (din),
-    .dout  (dout)
-  );
+  // Generate desired SRAM with IHP primitives
+  if(MEM_SIZE_WORDS == 2048 && WORD_SIZE == 32) begin: gen_1024x64xBx1
+    logic [63:0] wdata64, rdata64, bm64;
+    // logic sram_sel_d, sram_sel_q;
+    logic [63:0] bm_q;
 
+    always_comb begin : gen_bit_interleaving
+      for(int i = 0; i < WORD_SIZE; i++) begin
+        // duplicate each bit
+        bm64[2*i]        = bm[i] & ~addr[0];
+        bm64[2*i+1]      = bm[i] & addr[0];
+        wdata64[2*i]     = din[i] & bm64[2*i]; // even bits (active if addr LSB is 0)
+        wdata64[2*i+1]   = din[i] & bm64[2*i+1]; // odd bits  (active if addr LSB is 1)
+
+        if(~addr_lsb) begin
+          dout[i] = rdata64[2*i] & bm_q[2*i];   // even bits
+        end else begin
+          dout[i] = rdata64[2*i+1] & bm_q[2*i+1]; // odd bits
+        end
+      end
+    end
+
+    always_ff @(posedge clk) begin: bm_assign
+      if(valid & ~write) begin
+        bm_q <= bm64;
+        addr_lsb <= addr[0];
+      end
+    end
+
+    // LSB needed for read in next cycle
+    // assign sram_sel_d = addr[0];
+
+    // always_ff @(posedge clk) begin : proc_mem_sel_q
+    //   if(valid & ~write) sram_sel_q <= sram_sel_d;
+    //   //else               sram_sel_q <= '0;
+    // end
+
+    RM_IHPSG13_1P_1024x64_c2_bm_bist mem_inst (
+      .A_CLK        (clk),
+      .A_DLY        (1'b1),
+      .A_ADDR       (addr[10:1]),
+      .A_BM         (bm64),
+      .A_MEN        (valid),
+      .A_WEN        (write),
+      .A_REN        (~write),
+      .A_DIN        (wdata64),
+      .A_DOUT       (rdata64),
+      .A_BIST_CLK   (1'b0),
+      .A_BIST_ADDR  (11'd0),
+      .A_BIST_DIN   (64'd0),
+      .A_BIST_BM    (64'b0),
+      .A_BIST_MEN   (1'b0),
+      .A_BIST_WEN   (1'b0),
+      .A_BIST_REN   (1'b0),
+      .A_BIST_EN    (1'b0)
+    );
+  end
 endmodule
 
